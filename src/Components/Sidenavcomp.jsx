66fb071e203fb5321104da542f6087207a0c61d3
Basnetsuman4 <arunbasnet54@gmail.com>
2022-12-30 18:16:56 +0545

    Navbar components

---
+++ src/Components/Sidenavcomp.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import DescriptionIcon from '@mui/icons-material/Description';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+import './Sidenavcomp.css';
+
+export const Sidenavcomp = [
+{
+    title: 'Profile',
+    icon: <AccountCircleIcon/>,
+    link:"/profile"
+},
+{
+    title: 'Registration',
+    icon: <PersonAddIcon/>,
+    link:"/registration"
+},
+{
+    title: 'Results',
+    icon: <DescriptionIcon/>,
+    link:"/results"
+},
+{
+    title: 'Performance',
+    icon: <AutoGraphIcon/>,
+    link:"/perfromance"
+}
+// ,
+// {
+    
+//     title: 'Log Out',
+//     icon: <LogoutIcon/>,
+//     link:"/logout"
+// }
+
+]
