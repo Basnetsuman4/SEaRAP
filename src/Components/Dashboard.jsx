3a06089a59253a6e4badd260f1e9126560843fe0
Basnetsuman4 <arunbasnet54@gmail.com>
2022-12-30 18:15:58 +0545

    Dasboard sample

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Dashboard.css'
+import Sidenav from './Sidenav';
+
+
+
+const Dashboard = () => {
+  return (
+            <>
+                <Sidenav />
+            
+            
+                <div className="dashboard">
+                </div>                    
+          
+            
+            
+            
+            </>
+
+    )
+}
+
+
+export default Dashboard;
