a5442aad7ef6bd2ad5db7d2733f655f9fc705a60
Basnetsuman4 <arunbasnet54@gmail.com>
2023-01-02 00:31:24 +0545

    Dashboard jsx file

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import './Dashboard.css'
-import Sidenav from './Sidenav';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Icon } from '@mui/material';
+import Myimage from './image/Pro_logo.png';
 
 
 
 const Dashboard = () => {
   return (
             <>
-                <Sidenav />
             
             
                 <div className="dashboard">
+                    <div className='dash_header'>
+                    
+                        
+                                <div className='notification_div'>
+                                    <div className="badge">
+                                        <Icon id='bellIcon'>
+                                            <NotificationsIcon/>
+                                        </Icon>
+                                    </div>
+                                    
+                                </div>
+                                
+                                                    
+                                <div className="userName_div">
+                                    <div className="showUser">
+                                        <div className="userName">
+                                            <div className="name">Arun Basnet</div>               
+                                        </div>
+                                        <div className="userImage">
+                                             <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9j5kXSP12ylfgHx30qabxtDu0GAX1cm19TTaKZVA1hONj-t6MfsXechjGt6hyYoPBD4Y&usqp=CAU" alt="demo" rel="norefferer" />
+   
+                                        </div>
+                                    </div>
+                                </div>
+
+                            
+                        
+                    </div>
+
+                    <div className='dash_separator'>
+                    
+                    </div>
+
+                    <div className='dash_body'>
+                    </div>
+
                 </div>                    
           
             
