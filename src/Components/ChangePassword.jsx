b79070ee7a6a33e0e2613c315e80a6bde4ef179b
Basnetsuman4 <arunbasnet54@gmail.com>
2023-01-31 12:51:14 +0545

    Change Password JSX File

--- src/Components/ChangePassword.jsx
+++ src/Components/ChangePassword.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ChangePassword.css'
 import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const ChangePassword =()=> {
-      return (
+      
+    
+
+ const [state1, setstate1] = useState(false);
+ const [state2, setstate2] = useState(false);
+ const [state3, setstate3] = useState(false);
+ 
+ 
+ 
+ const toggleBtn1 = ()=>{
+    setstate1(prevState => !prevState);
+ }
+ const toggleBtn2 = ()=>{
+    setstate2(prevState => !prevState);
+ }
+ const toggleBtn3 = ()=>{
+    setstate3(prevState => !prevState);
+ }
+
+
+    
+    
+    return (
                 <>        
                     <div className="CP">
 
@@ -15,15 +39,36 @@ const ChangePassword =()=> {
                                 <Form>
                                     <FormGroup>
                                         <Label for="current-password">Current Password</Label>
-                                        <Input type="password" name="current-password" id="current-password"  />
+                                        <div className='ipic'>
+                                            <Input type={state1 ? "text" : "password"} name="current-password" id="currentPassword" /> 
+                                            <span onClick={toggleBtn1} >
+                                                {state1 ? <VisibilityOffIcon/>:
+                                                    <VisibilityIcon/>
+                                                }
+                                            </span>
+                                        </div>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label for="new-password">New Password</Label>
-                                        <Input type="password" name="new-password" id="new-password" placeholder="Enter new-password" />
+                                        <div className='ipic' >
+                                            <Input type={state2 ? "text" : "password"} name="new-password" id="new-password" placeholder="Enter new-password" />
+                                            <span onClick={toggleBtn2} >
+                                                {state2 ? <VisibilityOffIcon/>:
+                                                    <VisibilityIcon/>
+                                                }
+                                            </span>
+                                        </div>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label for="reenter-new-password">Re-Enter New Password</Label>
-                                        <Input type="password" name="reenter-new-password" id="reenter-new-password" placeholder="Re-Enter Username" />
+                                        <div className='ipic'>
+                                            <Input type={state3 ? "text" : "password"} name="reenter-new-password" id="reenter-new-password" placeholder="Re-Enter Username" />
+                                            <span onClick={toggleBtn3} >
+                                                {state3 ? <VisibilityOffIcon/>:
+                                                    <VisibilityIcon/>
+                                                }
+                                            </span>
+                                        </div>
                                     </FormGroup>            
                                                  
                                     <Button id='subbtn' >Submit</Button>
