d13066cfc85777a1a2ddcda4d9d8c1c286e48111
Basnetsuman4 <arunbasnet54@gmail.com>
2023-02-17 18:32:06 +0545

    Error Page JSX

---
+++ src/Components/Errpage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './errpage.css'
+export default function Errpage() {
+    return (
+        <>
+            <div className="body">
+                <div className="FPbody">
+                    <p>!!404!!</p>
+                    <br></br>
+                    <h1>Page Not Found</h1>
+                    <br></br>cd
+                    <input type="submit" value="GO BACK" name="" />
+                </div>
+            </div>
+        </>
+    )
+}
